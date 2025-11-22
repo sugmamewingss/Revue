2a7e216bd9494b395c8da6a403aa9ed9c2a7775c
sugmamewingss <alfiperdiansyah@gmail.com>
2025-11-22 20:53:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('items', ItemController::class);
+Route::resource('reviews', ReviewController::class);
+Route::resource('genres', GenreController::class);
+
